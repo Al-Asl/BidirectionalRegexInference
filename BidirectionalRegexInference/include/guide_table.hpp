6def1a117b822de77e62cc1c01cb2eb750bcc220
ahmad al-asl <ahmad.alasl@yahoo.com>
2026-02-10 14:21:27 +0000

    update the guideTable
    - guide table is now store the index of the bit instead of the bitmask
    - guide table has a new way to represent the concat relation throw adjacency list

--- BidirectionalRegexInference/include/guide_table.hpp
+++ BidirectionalRegexInference/include/guide_table.hpp
@@ -13,39 +13,42 @@ namespace rei
 
         class Iterator {
         public:
-            Iterator(const CS* p);
+            Iterator(const int* p);
 
-            Pair<CS> operator*() const;
+            Pair<int> operator*() const;
             Iterator& operator++();
             bool operator!=(const Iterator& solved) const;
         private:
-            const CS* ptr;
+            const int* ptr;
         };
 
         class RowIterator {
         public:
-            RowIterator(const CS* data, int gtColumns, int rowIndex);
+            RowIterator(const int* data, int gtColumns, int rowIndex);
             Iterator begin();
             Iterator end();
         private:
-            const CS* data;
+            const int* data;
             int row;
             int gtColumns;
         };
 
-        GuideTable(std::vector<std::vector<CS>> gt, int alphabetSize);
+        RowIterator IterateRow(int rowIndex) const;
+
+        GuideTable(std::vector<std::vector<int>> gt, int alphabetSize);
 
         GuideTable();
 
         ~GuideTable();
 
-        RowIterator IterateRow(int rowIndex) const;
-
         int ICsize;
         int gtColumns;
         int alphabetSize;
+        // adjacencyList[i][j].first : i the index of the first bit while .first is the index
+        // of the second bit and .second is the result of concatenation
+        std::vector<std::vector<std::pair<int, int>>> adjacencyList;
     private:
-        CS* data;
+        int* data;
     };
 
     bool generatingGuideTable(GuideTable& guideTable, CS& posBits, CS& negBits,
